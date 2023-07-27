1d84759ff92341bb51e6c4f2996e48db487ca9ac
FrBarbarossa <makushinsp@mail.ru>
2023-07-27 12:13:19 +0300

    Some dynamic forms tested. Made with jquery, but I suppose it`s not really safe way. btw it is working!

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,8 +3,13 @@ from .models import Person
 from django.urls import reverse
 from django.http import HttpResponseRedirect, JsonResponse
 from django.db.utils import IntegrityError
+from django.forms import formset_factory
+from .forms import *
+
+
 def index(request):
     return render(request, 'polls/index.html')
+
 
 def postcard(request):
     print(request.POST['pername'])
@@ -20,7 +25,20 @@ def postcard(request):
         print(error)
     return HttpResponseRedirect(reverse("polls:index"))
 
+
 def test_ajax(request):
     print("!!!!!")
     return JsonResponse({'status': 'Invalid request', "some_param": True}, status=200)
+
+
+def formset_test(request):
+    print("In formset_test")
+    if request.method == "GET":
+        pass
+        # formSet = formset_factory(SurnameForm, extra=4)
+    if request.method == "POST":
+        print(request.POST)
+        # formSet = formset_factory(NameForm, SurnameForm, extra=4)
+    return render(request, 'polls/form_test.html')
+
 # Create your views here.
